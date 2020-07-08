0e8a98d9eaa607f9419ace14bfd1613baf178366
João Vitor Fernandes Canuto <joao678@gmail.com>
2020-07-08 18:30:16 -0300

    Colocado aba transportadora

---
+++ src/app/transportadora/transportadora.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transportadora',
+  templateUrl: 'transportadora.page.html',
+  styleUrls: ['transportadora.page.scss']
+})
+export class TransportadoraPage {
+
+  constructor() {}
+
+}
